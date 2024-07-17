88ae4ae55701044b204c82ac521b1e6578c4b007
PokeFred <Cedric.paelmke@gmx.de>
2024-07-17 22:16:53 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<div>Hello World</div>
+<script lang="ts">
+    import { onMount } from "svelte"
+    import { getName, getVersion } from "@tauri-apps/api/app"
+
+    type App = {
+        name: string
+        version: string
+    }
+
+    let app: App = { name: "", version: "" }
+    onMount(async (): Promise<void> => {
+        app.name = await getName()
+        app.version = await getVersion()
+    })
+</script>
+
+<div>{app.name}</div>
+<div>Version: {app.version}</div>
