a3bde292cb46b256d2aa7ba519493d2aa5b8d59e
PokeFred <Cedric.paelmke@gmx.de>
2024-07-17 21:41:23 +0200

    add backend

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,16 @@
-import adapter from "@sveltejs/adapter-node"
+import adapter from "@sveltejs/adapter-static"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter({
-            out: "./build",
-            precompress: true,
-            envPrefix: ""
-        }),
+        adapter: adapter(),
         env: {
             publicPrefix: "PUBLIC_",
             privatePrefix: "PRIVATE_"
         },
         alias: {
-            $server: "./src/lib/server",
             $components: "./src/lib/components",
             $utils: "./src/lib/utils",
             $images: "./src/lib/images",
